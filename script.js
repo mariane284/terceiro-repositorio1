14b3d4ab0596ec9e71e691bf93cacdae49c1b5b3
mariane284 <132462688+mariane284@users.noreply.github.com>
2025-04-07 22:52:08 -0300

    07.04.25

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener(‘DOMContentLoaded’, function(){
+    const aumentaFonteBotao = document.getElementById(‘aumentarfonte’);
+    aumentaFonteBotao.addEventListener(‘click’, function(){
+    })
+   const botaoDeacessibilidade = Document.getElementById
+   ("botao-acessibilidade");
+   const opcoesDeacessibilidade =document.getElementById
+   ("opcoes-acessibilidade");
+})
