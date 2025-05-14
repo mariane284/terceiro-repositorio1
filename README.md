7c6ba3b82d8034456cf7a12ae80f2addd4c39536
mariane284 <132462688+mariane284@users.noreply.github.com>
2025-05-13 21:45:26 -0300

    13/05/25

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# terceiro-repositorio1
-criado para estudos de acessibilidade nas paginas web
+# site-acessivel
+Site de referência para desenvolver conteúdo acessível em páginas web
--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-document.addEventListener(‘DOMContentLoaded’, function(){
-    const aumentaFonteBotao = document.getElementById(‘aumentarfonte’);
-    aumentaFonteBotao.addEventListener(‘click’, function(){
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
     })
-   const botaoDeacessibilidade = Document.getElementById
-   ("botao-acessibilidade");
-   const opcoesDeacessibilidade =document.getElementById
-   ("opcoes-acessibilidade");
+    
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    })
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    })
 })
+
